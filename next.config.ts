df0382c1871f46e36032c534a9f8cebcc984e2cc
Deepraj Pagare <99066501+Deepraj0502@users.noreply.github.com>
2026-04-27 23:10:37 +0530

    Add TypeScript type to nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: "export",
   basePath: "/eis-toolkit",
   assetPrefix: "/eis-toolkit/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
