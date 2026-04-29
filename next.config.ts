e073799b3959743c50ce35b46cac736ad8f3f712
Deepraj Pagare <99066501+Deepraj0502@users.noreply.github.com>
2026-04-29 12:17:40 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/eis-toolkit",
-  assetPrefix: "/eis-toolkit/",
+  assetPrefix: "/eis-toolkit",
   trailingSlash: true,
   images: {
     unoptimized: true,
